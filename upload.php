ae6d97cc0e45a7bc338552f36e3e0034b564d0f3
Tshilidzi Tshivhula <tshilidzi@vontae.co.za>
2020-03-22 10:44:24 +0200

    upload

---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_GET['upload'])) {
+  $_FILES['mp3']['tmp_name'];
+
+  /*if ($_FILES['mp3']['type'] != 'audio/mpeg') {
+    die('Invalid file type' . $_FILES['mp3']['type']);
+    exit();
+  }*/
+
+  $name = $_FILES['mp3']['name'];
+
+  /*if (!preg_match("/.mp3/is", $name)) {
+    die('Invalid file type2' . $_FILES['mp3']['type']);
+    exit();
+  }*/
+
+  //$name = md5($name . time() . rand(0, 10)) . '.mp3';
+  $name = 'audio/' . $name;
+
+  copy($_FILES['mp3']['tmp_name'], $name) or
+    die('Failed to upload MP3 file...');
+  die('done');
+  exit();
+} else {
+  echo '<form method="post" action="upload.php?upload" enctype="multipart/form-data"><input type="file" name="mp3" type="submit" /> <input type="submit" /></form>';
+}
+?>
